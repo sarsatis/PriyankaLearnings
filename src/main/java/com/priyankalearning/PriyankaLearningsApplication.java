891dbccbbce85b2d48bad120bb397bdd5e2d4f1c
sarsatis <sarthak8055@gmail.com>
2023-03-23 15:55:22 +0000

    git inital commit

---
+++ src/main/java/com/priyankalearning/PriyankaLearningsApplication.java
@@ -0,0 +1,13 @@
+package com.priyankalearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriyankaLearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriyankaLearningsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyankalearning/PriyankaLearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyankalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriyankaLearningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
