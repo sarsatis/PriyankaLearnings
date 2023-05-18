1274f6c3601774005787a0083104b10fe8f5a34d
sarsatis <sarthak8055@gmail.com>
2023-05-18 21:19:50 +0100

    Update PriyankaController.java

--- src/main/java/com/priyankalearning/controller/PriyankaController.java
+++ src/main/java/com/priyankalearning/controller/PriyankaController.java
@@ -3,7 +3,7 @@ package com.priyankalearning.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/app1")
 public class PriyankaController {
 
     @GetMapping("/hello")
