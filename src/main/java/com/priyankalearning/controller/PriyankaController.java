db9b13beb4b895542d6a011dfdcc7da6ebada376
sarsatis <sarthak8055@gmail.com>
2023-05-19 10:28:07 +0100

    New feature

--- src/main/java/com/priyankalearning/controller/PriyankaController.java
+++ src/main/java/com/priyankalearning/controller/PriyankaController.java
@@ -8,7 +8,7 @@ public class PriyankaController {
 
     @GetMapping("/app1/hello")
     public String sayHello(){
-        return "Hi";
+        return "Hi Sarthak";
     }
 
     @GetMapping("/app1/bye")
