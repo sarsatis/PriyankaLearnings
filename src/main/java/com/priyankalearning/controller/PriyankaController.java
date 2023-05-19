18fe6fa3a68e420f02076547e81a5d787002fb20
sarsatis <sarthak8055@gmail.com>
2023-05-19 13:16:37 +0100

    sar

--- src/main/java/com/priyankalearning/controller/PriyankaController.java
+++ src/main/java/com/priyankalearning/controller/PriyankaController.java
@@ -8,7 +8,7 @@ public class PriyankaController {
 
     @GetMapping("/app1/hello")
     public String sayHello(){
-        return "Hi Gagan";
+        return "Hi Sarthak";
     }
 
     @GetMapping("/app1/bye")
